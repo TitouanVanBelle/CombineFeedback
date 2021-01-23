3c929c303230b524d77852ee0c4fdd5ee4267d6f
Titouan Van Belle <titouan.vanbelle@gmail.com>
2021-01-23 17:30:30 +0100

    Test

--- Sources/CombineFeedbackUI/Widget.swift
+++ Sources/CombineFeedbackUI/Widget.swift
@@ -4,17 +4,17 @@ import SwiftUI
 
 public struct Widget<State, Event, Content: View>: View {
     private let store: Store<State, Event>
-    private let content: (Context<State, Event>) -> Content
+    private let content: Content
 
     public init(
         store: Store<State, Event>,
         @ViewBuilder content: @escaping (Context<State, Event>) -> Content
     ) {
         self.store = store
-        self.content = content
+        self.content = content(store.context)
     }
 
     public var body: some View {
-        return content(store.context)
+        content
     }
 }
